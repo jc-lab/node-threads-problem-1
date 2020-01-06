57b7a4fb39f1d2bd51007ca011d6a54a75df7a6a
Jichan <development@jc-lab.net>
2020-01-06 21:58:54 +0900

    update

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import {spawn, Thread, Worker} from "threads"
 
-(async () => {
+setTimeout(async () => {
     const threaded = await spawn(new Worker('./worker.ts'));
     await threaded.doit();
     await Thread.terminate(threaded);
-})();
+}, 100);
--- worker.ts
+++ worker.ts
@@ -8,51 +8,90 @@ import axios from "axios";
 import httpAdapter from 'axios/lib/adapters/http';
 
 const mscabBeginPattern = Buffer.from([0x4d, 0x53, 0x43, 0x46, 0x00, 0x00, 0x00, 0x00]);
+const mscabCheckPattern = [0x4d, 0x53, 0x43, 0x46, 0x00, 0x00, 0x00, 0x00, -1, -1, -1, -1, 0x00, 0x00, 0x00, 0x00, -1, -1, -1, -1, 0x00, 0x00, 0x00, 0x00, 0x03, 0x00, 0x01, 0x00];
 
 class SoftpaqExtractTransform extends streams.Transform {
-    private _parent: streams.Readable;
-
     private _cabFound: number = 0;
     private _cabPatternIndex: number = 0;
+    private _cabBuffer: Buffer | null = null;
 
     private _totalReadBytes: number = 0;
 
-    constructor(parent: streams.Readable) {
+    private _loopCount = 0;
+
+    constructor() {
         super();
-        this._parent = parent;
     }
 
     _transform(chunk: any, encoding: string, callback: streams.TransformCallback): void {
         const buffer = chunk as Buffer;
+
         let offset = 0;
 
         while(offset < buffer.length)
         {
-            console.log("TP-1 : ", this._cabFound, " :: ", this._totalReadBytes, process.pid);
-
+            this._loopCount++;
+            console.log("this._loopCount = ", this._loopCount);
             if (this._cabFound == 0) {
                 const beginIndex = buffer.indexOf(mscabBeginPattern);
                 if (beginIndex >= 0) {
                     offset = beginIndex;
                     this._cabFound = 1;
                     this._cabPatternIndex = 0;
-                    console.log("BEGIN PATTERN FOUND: ", (this._totalReadBytes + offset));
-                    console.log("this._cabFound =>", this._cabFound);
                 }else{
-                    console.log("TP-1.5  ", beginIndex);
                     break;
                 }
             }
-            console.log("*********************************************************************************");
-            console.log("*********************************************************************************");
-            console.log("*********************************************************************************");
-            console.log("*********************************************************************************");
-            console.log("*********************************************************************************");
-            console.log("*********************************************************************************");
-            console.log("*********************************************************************************");
-            console.log("*********************************************************************************");
-            console.log("*********************************************************************************");
-            console.log("*********************************************************************************");
+            console.log("********** 1111111111 **********");
+
+            if (this._cabFound == 1) {
+                let i = this._cabPatternIndex;
+                let j = offset;
+                let matched = true;
+                for (; (i < mscabCheckPattern.length) && (j < buffer.length); i++, j++) {
+                    if ((mscabCheckPattern[i] >= 0) && (mscabCheckPattern[i] != buffer[j])) {
+                        matched = false;
+                        j++;
+                        break;
+                    }
+                }
+                if (matched) {
+                    process.exit(0);
+                    console.log("MATCHED!: ", (this._totalReadBytes + offset));
+                    console.log(`i == mscabCheckPattern.length : ${i} == ${mscabCheckPattern.length}`);
+                    this._cabPatternIndex = i;
+                    if (i == mscabCheckPattern.length) {
+                        this._cabFound = 2;
+                    } else {
+                        const subarr = buffer.subarray(offset);
+                        if (this._cabBuffer) {
+                            this._cabBuffer = Buffer.concat([this._cabBuffer, subarr]);
+                        } else {
+                            this._cabBuffer = subarr;
+                        }
+                    }
+                } else {
+                    offset = j;
+                    console.log("CANCEL MATCH: ", (this._totalReadBytes + offset));
+                    this._cabFound = 0;
+                }
+            }
+
+            console.log("TP-3");
+
+            if (this._cabFound == 2) {
+                if (this._cabBuffer) {
+                    this.push(this._cabBuffer);
+                    this._cabBuffer = null;
+                }
+                if (offset > 0) {
+                    this.push(buffer.subarray(offset));
+                } else {
+                    this.push(buffer);
+                }
+
+                break;
+            }
         }
 
         console.log("Loop out");
@@ -72,7 +111,7 @@ expose({
                 return new Promise((resolve, reject) => {
                     const fileout = fs.createWriteStream("D:\\temp\\sp74100.cab");
                     (response.data as streams.Readable)
-                        .pipe(new SoftpaqExtractTransform((response.data as streams.Readable)))
+                        .pipe(new SoftpaqExtractTransform())
                         .pipe(fileout)
                         // .pipe(cabinetExtract)
                         .on('error', (err) => {
